66375ec6ecad1f4e201fcca5467209201de380b1
arwin-dev <arwin.dev@outlook.com>
2024-02-14 20:05:53 -0500

    asdasdasdasd

--- rush.c
+++ rush.c
@@ -8,7 +8,7 @@
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\n"
 
-void printErrorMessage() {
+void error() {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
     fflush(stderr);
@@ -19,7 +19,7 @@ char *rushReadLine() {
     size_t bufsize = 0;
 
     if (getline(&input, &bufsize, stdin) == -1) {
-        printErrorMessage();
+        error();
         exit(EXIT_FAILURE);
     }
 
@@ -33,7 +33,7 @@ char **rushSplitLine(char *input, char *delim) {
     char *token=NULL;
 
     if (!tokens) {
-        printErrorMessage();
+        error();
         exit(EXIT_FAILURE);
     }
 
@@ -45,7 +45,7 @@ char **rushSplitLine(char *input, char *delim) {
             bufSize += TOK_BUFSIZE;
             tokens = realloc(tokens, bufSize * sizeof(char *));
             if (!tokens) {
-                printErrorMessage();
+                error();
                 exit(EXIT_FAILURE);
             }
         }
@@ -55,66 +55,26 @@ char **rushSplitLine(char *input, char *delim) {
     return tokens;
 }
 
-void rushExecuteSingleCommand(char **args, char **path)
-{   
-    for(int j = 0; path[j] != NULL; j++)
-    { 
-        size_t full_path_len = strlen(path[j]) + strlen("/") + strlen(args[0]) + 1;
-        char *full_path = (char *)malloc(full_path_len * sizeof(char));
-        if (full_path == NULL) {
-            printErrorMessage();
-            return;
-        }
-
-        strcpy(full_path, path[j]);
-        strcat(full_path, "/");
-        strcat(full_path, args[0]);
-        int fd = access(full_path, X_OK);
-        if (fd == -1) {
-            printErrorMessage();
-        }
-        else
-        {
-            int redir_count = 0;
-            int redir_index = -1;
-            for (int i = 0; args[i] != NULL; i++) {
-                if (strcmp(args[i], ">") == 0) {
-                    redir_count++;
-                    redir_index = i;
-                    if (redir_count > 1 || args[i+2] != NULL) {
-                        printErrorMessage();
-                        exit(EXIT_FAILURE);
-                    }
-                }
-            }
-
-            if (redir_index != -1 && args[redir_index + 1] != NULL)
-            {
-                int file_desc = open(args[redir_index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (file_desc == -1)
-                {
-                    printErrorMessage();
-                    exit(EXIT_FAILURE);
-                }
-                dup2(file_desc, STDOUT_FILENO);
-                close(file_desc);
-                args[redir_index] = NULL;
-                args[redir_index + 1] = NULL;
-            }
-
-            if (execvp(args[0], args) == -1)
-            {
-                printErrorMessage();
-            }
-        }
-        free(full_path);
+char **generatePath(char **args1) {
+    int size = 0;
+    while (args1[size] != NULL) {
+        size++;
     }
+    char **path = malloc(size * sizeof(char *));
+    if (!path) {
+        error();
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 1; args1[i] != NULL; i++) {
+        path[i - 1] = strdup(args1[i]);
+    }
+    path[size - 1] = NULL;
+    return path;
 }
 
-void rushExecute(char **args, char **path)
+void execute(char **args, char **path)
 {
     int success = 0;
-    size_t final_full_path_len = 0; 
     char *final_full_path = NULL;
     for(int j = 0; path[j] != NULL; j++)
     {
@@ -141,135 +101,76 @@ void rushExecute(char **args, char **path)
 
     if(success == 0)
     {
-        printErrorMessage();
+        error();
         return;
     }
     else
     {
-        pid_t pid;
-        int status;
-
-        pid = fork();
-        if(pid == 0)
-        {
-            int redir_count = 0;
-            int redir_index = -1;
-            for (int i = 0; args[i] != NULL; i++) {
-                if (strcmp(args[i], ">") == 0) {
-                    redir_count++;
-                    redir_index = i;
-                    if (redir_count > 1 || args[i+1] == NULL ||args[i+2] != NULL ) {
-                        printErrorMessage();
-                        exit(EXIT_FAILURE);
-                    }
-                }
-            }
-
-            if (redir_index != -1 && args[redir_index + 1] != NULL) {
-                int file_desc = open(args[redir_index + 1], 1 | 512 | 64, 0644);
-                if (file_desc == -1) {
-                    printErrorMessage();
+        int redir_count = 0;
+        int redir_index = -1;
+        for (int i = 0; args[i] != NULL; i++) {
+            if (strcmp(args[i], ">") == 0) {
+                redir_count++;
+                redir_index = i;
+                if (redir_count > 1 || args[i+1] == NULL || args[i+2] != NULL ) {
+                    error();
                     exit(EXIT_FAILURE);
                 }
-                dup2(file_desc, STDOUT_FILENO);
-                close(file_desc);
-                args[redir_index] = NULL;
-                args[redir_index + 1] = NULL;
-            }
-
-            if(execvp(final_full_path, args) == -1){
-                printErrorMessage();
             }
         }
-        else if (pid < 0)
-        {
-            printErrorMessage();
-        }
-        else
-        {
-            wait(&status);
-            return;
-        }
-    }
-}
 
-void rushExecuteASDASD(char **args, char **path)
-{
-
-    for(int j = 0; args[j] != NULL; j++)
-        printf("%s\n", args[j]);
-
-    int num_commands = 0;
-    char *commands[64];
-
-    if (args[0] != NULL) {
-        commands[num_commands++] = args[0]; 
-    }
-    
-    for (int i = 0; args[i] != NULL; i++)
-    {
-        if (strcmp(args[i], "&") == 0)
-        {
-            args[i] = NULL;                          
-            if (args[i + 1] != NULL) {
-                commands[num_commands++] = args[i + 1]; // Include next token as command
+        if (redir_index != -1 && args[redir_index + 1] != NULL) {
+            int file_desc = open(args[redir_index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (file_desc == -1) {
+                error();
+                exit(EXIT_FAILURE);
             }
+            dup2(file_desc, STDOUT_FILENO);
+            close(file_desc);
+            args[redir_index] = NULL;
+            args[redir_index + 1] = NULL;
         }
-        else {
-            commands[num_commands++] = args[i]; // Include current token as command
-        }
-    }
-    commands[num_commands++] = NULL; 
 
-    pid_t pids[num_commands];
-    for (int i = 0; i < num_commands; i++)
-    {
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            printErrorMessage();
-            return;
-        }
-        else if (pid == 0)
-        {
-            rushExecuteSingleCommand(rushSplitLine(commands[i], TOK_DELIM), path);
+        if(execv(final_full_path, args) == -1){
+            error();
             exit(EXIT_FAILURE);
         }
-        else
-        {
-            pids[i] = pid;
-        }
-    }
-
-    for (int i = 0; i < num_commands; i++)
-    {
-        int status;
-        waitpid(pids[i], &status, 0);
     }
 }
 
-char **generatePath(char **args1) {
-    int size = 0;
-    while (args1[size] != NULL) {
-        size++;
+char *checkIfValidCommand(char **args, char **path)
+{
+    char *final_full_path = NULL;
+    for(int j = 0; path[j] != NULL; j++)
+    {
+        size_t full_path_len = strlen(path[j]) + strlen("/") + strlen(args[0]) + 1;
+        char *full_path = (char *)malloc(full_path_len * sizeof(char));
+        if (full_path == NULL) 
+        {
+            continue;
+        }
+
+        strcpy(full_path, path[j]);
+        strcat(full_path, "/");
+        strcat(full_path, args[0]);
+        int checkIfFileExists = access(full_path, X_OK);
+        if(checkIfFileExists == 0)
+        {
+            if(final_full_path != NULL) free(final_full_path);
+            final_full_path = (char *)malloc(full_path_len * sizeof(char));
+            strcpy(final_full_path, full_path);
+            break;
+        }
     }
-    char **path = malloc(size * sizeof(char *));
-    if (!path) {
-        printErrorMessage();
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 1; args1[i] != NULL; i++) {
-        path[i - 1] = strdup(args1[i]);
-    }
-    path[size - 1] = NULL;
-    return path;
+
+    return final_full_path;
 }
 
 int main(int argc, char *argv[]) 
 {
     if(argc > 1)
     {
-        printErrorMessage();
+        error();
         return EXIT_FAILURE;
     }
     
@@ -277,13 +178,9 @@ int main(int argc, char *argv[])
     char **args;
     char *initialPath[2];
     initialPath[0] = strdup("/bin");
-    initialPath[1] = strdup("/usr/bin");
-    initialPath[2] = NULL;
+    initialPath[1] = NULL;
 
     char **path = initialPath;
-
-    // for(int j = 0; path[j] != NULL; j++)
-    //     printf("%s\n", path[j]);
 
     while (1) {
         printf("rush> ");
@@ -292,12 +189,15 @@ int main(int argc, char *argv[])
         input = rushReadLine();
         args = rushSplitLine(input, "&");
 
+        int num_args = 0;
         for(int i = 0; args[i] != NULL; i++)
         {
-            char **parall=NULL;
+            pid_t pid;
+            int status;
+            char **parall = NULL;
             parall = rushSplitLine(args[i], TOK_DELIM);
 
-            if(parall[0] == NULL)
+            if(parall[0] == NULL )
             {
                 continue;
             }
@@ -306,12 +206,16 @@ int main(int argc, char *argv[])
                 if(parall[1] == NULL)
                     exit(1);
                 else
-                    printErrorMessage();
+                    error();
             } 
             else if (strcmp(parall[0], "cd") == 0) 
             {
-                if (chdir(parall[1]) != 0) {
-                    printErrorMessage();
+                if (parall[1] == NULL) {
+                    error(); 
+                } else {
+                    if (chdir(parall[1]) != 0) {
+                        error(); 
+                    }
                 }
             } 
             else if (strcmp(parall[0], "path") == 0) 
@@ -320,13 +224,34 @@ int main(int argc, char *argv[])
             }
             else
             {
-                if(path[0] == NULL)
+                if(path[0] == NULL || checkIfValidCommand(parall, path) == NULL)
                 {
-                    printErrorMessage();
+                    error();
                     continue;
                 }
-                rushExecute(parall, path);
+                pid = fork();
+                if (pid == -1)
+                {
+                    error();
+                }
+                else if(pid == 0)
+                {
+                    execute(parall, path);
+                }
+                else if(pid < 0)
+                {
+                    error();
+                }
+                else
+                {
+                    num_args++;
+                }
             }
+        }
+
+        for (int i = 0; i < num_args; i++) {
+            int status;
+            wait(&status); 
         }
 
         free(input);
