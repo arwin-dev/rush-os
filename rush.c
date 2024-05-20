bc9d202ba65c087a571dc81362d5a75d26be0b17
arwin-dev <arwin.dev@outlook.com>
2024-05-20 12:17:46 -0400

    final

--- rush.c
+++ rush.c
@@ -8,13 +8,15 @@
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\n"
 
+// Function to print error message
 void error() {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
     fflush(stderr);
 }
 
-char *rushReadLine() {
+// Function to read a line of input from stdin
+char *readLine() {
     char *input = NULL;
     size_t bufsize = 0;
 
@@ -26,7 +28,8 @@ char *rushReadLine() {
     return input;
 }
 
-char **rushSplitLine(char *input, char *delim) {
+// Function to split a line into tokens based on a delimiter
+char **splitLine(char *input, char *delim) {
     int bufSize = TOK_BUFSIZE;
     int i = 0;
     char **tokens = malloc(bufSize * sizeof(char *));
@@ -55,6 +58,7 @@ char **rushSplitLine(char *input, char *delim) {
     return tokens;
 }
 
+// Function to generate a new path based on the input arguments
 char **generatePath(char **args1) {
     int size = 0;
     while (args1[size] != NULL) {
@@ -72,72 +76,7 @@ char **generatePath(char **args1) {
     return path;
 }
 
-void execute(char **args, char **path)
-{
-    int success = 0;
-    char *final_full_path = NULL;
-    for(int j = 0; path[j] != NULL; j++)
-    {
-        size_t full_path_len = strlen(path[j]) + strlen("/") + strlen(args[0]) + 1;
-        char *full_path = (char *)malloc(full_path_len * sizeof(char));
-        if (full_path == NULL) 
-        {
-            continue;
-        }
-
-        strcpy(full_path, path[j]);
-        strcat(full_path, "/");
-        strcat(full_path, args[0]);
-        int checkIfFileExists = access(full_path, X_OK);
-        if(checkIfFileExists == 0)
-        {
-            success = 1;
-            if(final_full_path != NULL) free(final_full_path);
-            final_full_path = (char *)malloc(full_path_len * sizeof(char));
-            strcpy(final_full_path, full_path);
-            break;
-        }
-    }
-
-    if(success == 0)
-    {
-        error();
-        return;
-    }
-    else
-    {
-        int redir_count = 0;
-        int redir_index = -1;
-        for (int i = 0; args[i] != NULL; i++) {
-            if (strcmp(args[i], ">") == 0) {
-                redir_count++;
-                redir_index = i;
-                if (redir_count > 1 || args[i+1] == NULL || args[i+2] != NULL ) {
-                    error();
-                    exit(EXIT_FAILURE);
-                }
-            }
-        }
-
-        if (redir_index != -1 && args[redir_index + 1] != NULL) {
-            int file_desc = open(args[redir_index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (file_desc == -1) {
-                error();
-                exit(EXIT_FAILURE);
-            }
-            dup2(file_desc, STDOUT_FILENO);
-            close(file_desc);
-            args[redir_index] = NULL;
-            args[redir_index + 1] = NULL;
-        }
-
-        if(execv(final_full_path, args) == -1){
-            error();
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
+// Function to check if a command is valid by searching for it in the specified paths using the "access" function
 char *checkIfValidCommand(char **args, char **path)
 {
     char *final_full_path = NULL;
@@ -147,6 +86,7 @@ char *checkIfValidCommand(char **args, char **path)
         char *full_path = (char *)malloc(full_path_len * sizeof(char));
         if (full_path == NULL) 
         {
+            free(final_full_path);
             continue;
         }
 
@@ -166,6 +106,47 @@ char *checkIfValidCommand(char **args, char **path)
     return final_full_path;
 }
 
+// Function to execute a command with the "execv" function
+void execute(char **args, char **path)
+{
+    char *final_full_path = checkIfValidCommand(args, path);
+
+    int redir_count = 0;
+    int redir_index = -1;
+    // checks if args has ">" and if it does, it checks if its valid command
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], ">") == 0) {
+            redir_count++;
+            redir_index = i;
+            if (redir_count > 1 || args[i+1] == NULL || args[i+2] != NULL ) {
+                error();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    // handles redirection here 
+    // args[redir_index + 1] -> this is the filename of the file you want to dump the data in
+    if (redir_index != -1 && args[redir_index + 1] != NULL) {
+        int file_desc = open(args[redir_index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (file_desc == -1) {
+            error();
+            exit(EXIT_FAILURE);
+        }
+        dup2(file_desc, STDOUT_FILENO);
+        close(file_desc);
+        args[redir_index] = NULL;
+        args[redir_index + 1] = NULL;
+    }
+
+    // executes the command 
+    if(execv(final_full_path, args) == -1){
+        error();
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Main function
 int main(int argc, char *argv[]) 
 {
     if(argc > 1)
@@ -186,49 +167,56 @@ int main(int argc, char *argv[])
         printf("rush> ");
         fflush(stdout);
 
-        input = rushReadLine();
-        args = rushSplitLine(input, "&");
+        input = readLine();
+        args = splitLine(input, "&");
 
+        // Initialize counter for child processes
         int num_args = 0;
+
         for(int i = 0; args[i] != NULL; i++)
         {
             pid_t pid;
-            int status;
-            char **parall = NULL;
-            parall = rushSplitLine(args[i], TOK_DELIM);
+            char **command = NULL;
+            command = splitLine(args[i], TOK_DELIM);
 
-            if(parall[0] == NULL )
+            // If the command is empty, skip it
+            if(command[0] == NULL )
             {
                 continue;
             }
-            if (strcmp(parall[0], "exit") == 0) 
+            // Execute built-in commands (exit)
+            else if (strcmp(command[0], "exit") == 0) 
             {
-                if(parall[1] == NULL)
+                if(command[1] == NULL)
                     exit(1);
                 else
                     error();
-            } 
-            else if (strcmp(parall[0], "cd") == 0) 
+            }
+            // Execute built-in commands (cd)
+            else if (strcmp(command[0], "cd") == 0) 
             {
-                if (parall[1] == NULL) {
+                if (command[1] == NULL) {
                     error(); 
                 } else {
-                    if (chdir(parall[1]) != 0) {
+                    if (chdir(command[1]) != 0) {
                         error(); 
                     }
                 }
-            } 
-            else if (strcmp(parall[0], "path") == 0) 
+            }
+            // Execute built-in commands (path)
+            else if (strcmp(command[0], "path") == 0) 
             {
-                path = generatePath(parall);
+                path = generatePath(command);
             }
             else
             {
-                if(path[0] == NULL || checkIfValidCommand(parall, path) == NULL)
+                //checks if the path is not NULL or if checks if the command is a valid executable in the path
+                if(path[0] == NULL || checkIfValidCommand(command, path) == NULL)
                 {
                     error();
                     continue;
                 }
+                // Fork a child process
                 pid = fork();
                 if (pid == -1)
                 {
@@ -236,7 +224,8 @@ int main(int argc, char *argv[])
                 }
                 else if(pid == 0)
                 {
-                    execute(parall, path);
+                    // Child process: execute the command
+                    execute(command, path);
                 }
                 else if(pid < 0)
                 {
@@ -244,11 +233,13 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
+                    // Parent process: increment child process counter
                     num_args++;
                 }
             }
         }
 
+        // Wait for all child processes to finish
         for (int i = 0; i < num_args; i++) {
             int status;
             wait(&status); 
